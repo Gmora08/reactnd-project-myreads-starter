cb8c8718bd8105701b9e95b596185cd3682c5e42
Gustavo Mora Gonzalez <gmora008@gmail.com>
2017-07-20 00:52:21 -0500

    feat: create Library component
    Create and add Library component in App.js
    
    Created in order to make more composable and maintainable code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as BooksAPI from './BooksAPI'
-import Bookshelf from './Bookshelf'
+import Library from './Library'
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -39,21 +39,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <Bookshelf shelfTitle="Currently Reading" />
-                <Bookshelf shelfTitle="Want to Read" />
-                <Bookshelf shelfTitle="Read" />
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
+          <Library />
         )}
       </div>
     )
---
+++ src/Library.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default class Library extends Component {
+
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <Bookshelf shelfTitle="Currently Reading" />
+            <Bookshelf shelfTitle="Want to Read" />
+            <Bookshelf shelfTitle="Read" />
+          </div>
+        </div>
+        <div className="open-search">
+          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+        </div>
+      </div>
+    );
+  }
+}
