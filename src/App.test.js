75c08d8bb28288ec99509009329d4f14d126ab50
Gustavo Mora Gonzalez <gmora008@gmail.com>
2017-07-20 00:41:20 -0500

    test: add Book component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import Book from './Book'
 
-/** 
- This course is not designed to teach Test Driven Development. 
- Feel free to use this file to test your application, but it 
+/**
+ This course is not designed to teach Test Driven Development.
+ Feel free to use this file to test your application, but it
  is not required.
 **/
 
@@ -13,4 +14,12 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div)
 })
 
-
+it('render Book component without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <Book
+      backgroundImage="http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api"
+      title="1Q84"
+      author="Haruki Murakami"
+    />, div)
+})
