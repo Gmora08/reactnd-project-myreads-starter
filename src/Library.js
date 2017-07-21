96c36e6ed57118d5b702a8a49e07ecab2e953d46
Gustavo Mora Gonzalez <gmora008@gmail.com>
2017-07-21 00:06:25 -0500

    feat: add link to /search route in Library component

--- src/Library.js
+++ src/Library.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Bookshelf from './Bookshelf'
 
@@ -22,7 +23,7 @@ export default class Library extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to="/search">Add a book</Link>
         </div>
       </div>
     );
